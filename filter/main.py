8a922c1c6aa4d41ce439b3ee2eab4a3b555cff89
Oguzhan Cagirir <oguzhan.cagirir@kcl.ac.uk>
2026-02-21 07:50:56 +0000

    feat(filter): add stub of the filter inference service entry point

---
+++ filter/main.py
@@ -0,0 +1,28 @@
+"""
+SentinelAI Filter Service - Entry Point
+
+This will serve as the service wrapper for the BERT filter.
+To be implemented.
+"""
+
+# pylint: disable=wrong-import-position
+
+# NOTE: Only disable C0413 if maintaining consistency between files manually or using ruff.
+
+import sys
+from pathlib import Path
+
+# Add parent directory to path to allow importing models and services
+sys.path.append(str(Path(__file__).parent.parent))
+
+# import config
+# from services.model_factory import load_production_model
+
+
+def init_app():
+    """Initialise the application components."""
+    return  # Stub
+
+
+if __name__ == "__main__":
+    init_app()
