d8b68e78b9c5ef231e76285eb58e8ba22534a6d3
Mariam Hafiz (k23115695) <mariam.hafiz@kcl.ac.uk>
2026-03-07 22:14:36 +0000

    feat(api): add Message and IncidentScore models

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,12 +2,14 @@ from api.models.auth_user import AuthUser
 from api.models.base import Base
 from api.models.company import Company
 from api.models.google_mailbox import GoogleMailbox
+from api.models.message import IncidentScore, Message
 from api.models.slack import SlackAccount, SlackWorkspace
 from api.models.subscription import Subscription
 from api.models.subscription_plan import SubscriptionPlan
 from api.models.user import User
 
 __all__ = [
-    "AuthUser", "Base", "Company", "GoogleMailbox", "SlackAccount",
-    "SlackWorkspace", "Subscription", "SubscriptionPlan", "User",
+    "AuthUser", "Base", "Company", "GoogleMailbox", "IncidentScore",
+    "Message", "SlackAccount", "SlackWorkspace", "Subscription",
+    "SubscriptionPlan", "User",
 ]
