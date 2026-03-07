5a240aa676c075a5a3f4cda989d25062b11ee10b
Mariam Hafiz (k23115695) <mariam.hafiz@kcl.ac.uk>
2026-03-07 22:32:07 +0000

    feat(api): add Payment and StripeEvent models for payments integration

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,6 +3,7 @@ from api.models.base import Base
 from api.models.company import Company
 from api.models.google_mailbox import GoogleMailbox
 from api.models.message import IncidentScore, Message
+from api.models.payment import Payment, StripeEvent
 from api.models.slack import SlackAccount, SlackWorkspace
 from api.models.subscription import Subscription
 from api.models.subscription_plan import SubscriptionPlan
@@ -10,6 +11,6 @@ from api.models.user import User
 
 __all__ = [
     "AuthUser", "Base", "Company", "GoogleMailbox", "IncidentScore",
-    "Message", "SlackAccount", "SlackWorkspace", "Subscription",
-    "SubscriptionPlan", "User",
+    "Message", "Payment", "SlackAccount", "SlackWorkspace", "StripeEvent",
+    "Subscription", "SubscriptionPlan", "User",
 ]
